690f0e591f4620142772774e22ffe199dc59362c
Nadia <nadiahosisy@gmail.com>
2024-12-15 21:54:34 +0200

    changed font

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -25,16 +25,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/about": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/about.js"
-    ],
-    "/menu-coffee": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/menu-coffee.js"
-    ],
     "/services": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{\"pages\\\\menu-coffee.jsx -> @/src/components/CoffeeMenu\":{\"id\":\"pages\\\\menu-coffee.jsx -> @/src/components/CoffeeMenu\",\"files\":[\"static/chunks/src_components_CoffeeMenu_js.js\"]}}"
+self.__REACT_LOADABLE_MANIFEST="{}"
