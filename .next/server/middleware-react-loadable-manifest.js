1c34e2e03045b15a789f28f34d9ce109972a865d
Nadia <nadiahosisy@gmail.com>
2024-12-16 21:56:14 +0200

    add css changes

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{}"
+self.__REACT_LOADABLE_MANIFEST='{"pages\\\\gallery.jsx -> @/src/components/PhotoGalleryIsotope":{"id":200,"files":["static/chunks/334.cf0d47a541698fff.js","static/chunks/200.36fdc6dc4f36eb56.js"]},"pages\\\\menu-coffee.jsx -> @/src/components/CoffeeMenu":{"id":5166,"files":["static/chunks/334.cf0d47a541698fff.js","static/chunks/166.165401366a5e51c4.js"]}}';
