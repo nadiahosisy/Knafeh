e89a2cc6d3d461d728ae0a78ac6c50506cbbb03b
Ward Aboroken <wardaboroken@gmail.com>
2024-12-16 22:06:50 +0200

    change

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
+=======
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {},
+  "appUsingSizeAdjust": false,
+  "pagesUsingSizeAdjust": false
+}
+>>>>>>> f0dfc22 (change)
