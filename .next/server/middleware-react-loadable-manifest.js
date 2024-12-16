7725f5a08d4b97f83cdf2423775c688598214e8c
Ward Aboroken <wardaboroken@gmail.com>
2024-12-16 22:06:36 +0200

    merge with main

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 self.__REACT_LOADABLE_MANIFEST='{"pages\\\\gallery.jsx -> @/src/components/PhotoGalleryIsotope":{"id":200,"files":["static/chunks/334.cf0d47a541698fff.js","static/chunks/200.36fdc6dc4f36eb56.js"]},"pages\\\\menu-coffee.jsx -> @/src/components/CoffeeMenu":{"id":5166,"files":["static/chunks/334.cf0d47a541698fff.js","static/chunks/166.165401366a5e51c4.js"]}}';
+=======
+self.__REACT_LOADABLE_MANIFEST={"menu-coffee.jsx -> @/src/components/CoffeeMenu":{"id":"menu-coffee.jsx -> @/src/components/CoffeeMenu","files":["static/chunks/src_components_CoffeeMenu_js.js"]}}
+>>>>>>> 7b1601d (merge with main)
